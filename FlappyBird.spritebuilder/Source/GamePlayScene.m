cd5793eb8e8939c912fa0cabc0b12d7cbbc37779
bednarekluc <luc@bednarek.be>
2015-06-14 10:54:39 +0200

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -17,4 +17,26 @@
 
 // put new methods here
 
+- (void)touchBegan:(CCTouch *)touch withEvent:(CCTouchEvent *)event {
+    // this will get called every time the player touches the screen
+    [character flap];
+}
+
+-(void) doSomething
+{
+    NSLog(@"Hello World!");
+}
+
+-(int) doSomethingWithAString: (NSString*) myString
+{
+    //returns the length of the string times ten
+    return [myString length] * 10;
+}
+
+-(NSArray *) addThisStringToAnArray:(NSString*) firstString andThisString:(NSString*) secondString andAlsoThisString:(NSString*) thirdString
+{
+    return [NSArray arrayWithObjects: firstString, secondString, thirdString, nil];
+}
+
+
 @end
